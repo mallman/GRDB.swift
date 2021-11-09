f92fa76afc181f8ac86ea8726460ccc30f69881e
Michael Allman <msa@allman.ms>
2021-11-09 11:31:23 -0800

    Rebase on v5.17.0

--- SQLiteCustom/GRDB.h
+++ SQLiteCustom/GRDB.h
@@ -6,6 +6,6 @@ FOUNDATION_EXPORT double GRDB_VersionNumber;
 //! Project version string for GRDB.
 FOUNDATION_EXPORT const unsigned char GRDB_VersionString[];
 
-#import <GRDB/GRDBCustomSQLite-USER.h>
-#import <GRDB/sqlite3.h>
+#import "GRDBCustomSQLite-USER.h"
+#import "sqlite3.h"
 #import "GRDB-Bridging.h"
